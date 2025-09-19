3a3afec5d07752aeb3059209a452bcb6d50928e5
Patricia Johansson <pj222uc@student.lnu.se>
2025-09-19 12:17:39 +0200

    Update day counter

--- src/DayCounter.js
+++ src/DayCounter.js
@@ -4,6 +4,8 @@
  * @version 1.0.0
  */
 
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24
+
 /**
  * Represents a DayCounter.
  */
@@ -15,24 +17,23 @@ export default class DayCounter {
    * @returns {number} The number of days.
    */
   getDays (date) {
-    const dateNow = Date.now()
+    const dateNow = this.#getMidnightTimestamp(Date.now())
+    const targetDate = this.#getMidnightTimestamp(date)
 
     // Time difference will be positive if the target date is in the future.
-    const timeDifference = this.#getMidnight(date) - this.#getMidnight(dateNow)
+    const timeDifference = targetDate - dateNow
 
-    const millisecondsPerDay = 1000 * 60 * 60 * 24
-    let days = timeDifference / millisecondsPerDay
-    days = days >= 0 ? Math.ceil(days) : Math.floor(days)
-    return Math.abs(days)
+    const days = timeDifference / MILLISECONDS_PER_DAY
+    return days >= 0 ? Math.ceil(days) : Math.floor(days)
   }
 
   /**
-   * Get the date with the time set to midnight.
+   * Get the timestamp for midnight of the specified date.
    *
    * @param {Date} date - The specified date.
    * @returns {Date} The date with the time set to midnight.
    */
-  #getMidnight (date) {
+  #getMidnightTimestamp (date) {
     const targetDate = new Date(date)
     return targetDate.setHours(0, 0, 0, 0)
   }
