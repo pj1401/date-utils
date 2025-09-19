0aa2d72b7dac979fa2df212259a65e05a761d292
Patricia Johansson <pj222uc@student.lnu.se>
2025-09-19 16:22:31 +0200

    Add formula for week number

--- src/WeekCounter.js
+++ src/WeekCounter.js
@@ -4,6 +4,8 @@
  * @version 1.0.0
  */
 
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24
+
 /**
  * Represents a WeekCounter.
  */
@@ -14,7 +16,37 @@ export default class WeekCounter {
    * @param {Date} date - The specified date.
    * @returns {number} The week number.
    */
-  getDays (date) {
-    return null
+  getWeekNumber (date) {
+    /**
+     * @see https://en.wikipedia.org/wiki/ISO_week_date#Calculating_the_week_number_from_an_ordinal_date
+     */
+    const dayOfTheYear = this.#getDayOfTheYear(date)
+    const dayOfTheWeek = this.#getDayOfTheWeek(date)
+    const week = (10 + (dayOfTheYear - dayOfTheWeek)) / 7
+    return week
+  }
+
+  /**
+   * Get the day of the year.
+   *
+   * @param {Date} date - The specified date.
+   * @returns {number} The day of the year.
+   */
+  #getDayOfTheYear (date) {
+    // Use UTC to avoid daylight savings problems.
+    const startOfTheYear = new Date.UTC(date.getFullYear(), 0, 1)
+    const targetDate = new Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+    return (targetDate - startOfTheYear) / MILLISECONDS_PER_DAY
+  }
+
+  /**
+   * Get the day of the week, represented by a number.
+   *
+   * @param {Date} date - The specified date.
+   * @returns {number} A number that represents the day of the week. 1 represents Monday, 7 for Sunday.
+   */
+  #getDayOfTheWeek (date) {
+    // getDay() returns 0 for Sunday, but Sunday has to be 7 for the formula to work.
+    return date.getDay() !== 0 ? date.getDay() : 7
   }
 }
