c282c209689039a9a73dd04615301cbd82b1fea4
Patricia Johansson <pj222uc@student.lnu.se>
2025-09-26 12:20:37 +0200

    Add more tests for getWeekNumber

--- test/DateUtil.test.js
+++ test/DateUtil.test.js
@@ -54,6 +54,12 @@ describe('getWeekNumber', () => {
       expect(dateUtil.getWeekNumber(date)).toEqual(52)
     })
 
+    test('3 January 2022, Monday, returns 1', () => {
+      const date = new Date('2022-01-03')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(1)
+    })
+
     test('1 January 2024, Monday, returns 1', () => {
       const date = new Date('2024-01-01')
       const dateUtil = new DateUtil()
@@ -72,6 +78,12 @@ describe('getWeekNumber', () => {
       expect(dateUtil.getWeekNumber(date)).toEqual(53)
     })
 
+    test('4 January 2027, Monday, returns 1', () => {
+      const date = new Date('2027-01-04')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(1)
+    })
+
     test('1 January 2033, Saturday, returns 53', () => {
       const date = new Date('2033-01-01')
       const dateUtil = new DateUtil()
@@ -80,6 +92,12 @@ describe('getWeekNumber', () => {
   })
 
   describe('End of the year', () => {
+    test('27 December 2004, Monday, returns 53', () => {
+      const date = new Date('2004-12-27')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(53)
+    })
+
     test('31 December 2004, Friday, returns 53', () => {
       const date = new Date('2004-12-31')
       const dateUtil = new DateUtil()
@@ -92,8 +110,20 @@ describe('getWeekNumber', () => {
       expect(dateUtil.getWeekNumber(date)).toEqual(1)
     })
 
+    test('28 December 2015, Monday, returns 53', () => {
+      const date = new Date('2015-12-28')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(53)
+    })
+
     test('31 December 2015, Thursday, returns 53', () => {
       const date = new Date('2015-12-31')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(53)
+    })
+
+    test('28 December 2020, Monday, returns 53', () => {
+      const date = new Date('2020-12-28')
       const dateUtil = new DateUtil()
       expect(dateUtil.getWeekNumber(date)).toEqual(53)
     })
@@ -104,10 +134,22 @@ describe('getWeekNumber', () => {
       expect(dateUtil.getWeekNumber(date)).toEqual(53)
     })
 
+    test('25 December 2023, Monday, returns 52', () => {
+      const date = new Date('2023-12-25')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(52)
+    })
+
     test('31 December 2023, Sunday, returns 52', () => {
       const date = new Date('2023-12-31')
       const dateUtil = new DateUtil()
       expect(dateUtil.getWeekNumber(date)).toEqual(52)
+    })
+
+    test('29 December 2025, Monday, returns 1', () => {
+      const date = new Date('2025-12-29')
+      const dateUtil = new DateUtil()
+      expect(dateUtil.getWeekNumber(date)).toEqual(1)
     })
 
     test('31 December 2025, Wednesday, returns 1', () => {
